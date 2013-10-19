00a16fc0f2a8cb9f64d0e53f78c8ec1e9a9a9416
Andrew Rice <acr31@cam.ac.uk>
2013-10-19 16:41:36 +0100

    Send a sensible error message if you cannot get a connection from the connection pool (e.g. if its been drained).  Added warning messages if we fail to commit the transaction and have to rollback

--- src/main/java/uk/ac/cam/cl/dtg/teaching/hibernate/HibernateSessionRequestFilter.java
+++ src/main/java/uk/ac/cam/cl/dtg/teaching/hibernate/HibernateSessionRequestFilter.java
@@ -12,14 +12,19 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
 
 import org.hibernate.HibernateException;
 import org.hibernate.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HibernateSessionRequestFilter implements Filter {
 
 	private HibernateUtil hibernateUtil;
 
+	private static Logger log = LoggerFactory.getLogger(HibernateSessionRequestFilter.class);
+	
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
 		hibernateUtil = HibernateUtil.getInstance();
@@ -30,7 +35,15 @@ public class HibernateSessionRequestFilter implements Filter {
 			FilterChain chain) throws IOException, ServletException {
 
 		if (hibernateUtil.isReady()) {
-			hibernateUtil.getSF().getCurrentSession().beginTransaction();
+			try {
+				hibernateUtil.getSF().getCurrentSession().beginTransaction();
+			} catch (HibernateException e) {
+				// failed to get a new transaction - one reason for this might
+				// be that the connection pool is empty
+				log.error("Unable to open a database connection");
+				((HttpServletResponse)response).sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,"Unable to open a database connection");
+				return;
+			}
 		}
 
 		chain.doFilter(request, response);
@@ -42,11 +55,16 @@ public class HibernateSessionRequestFilter implements Filter {
 				t.commit();
 			} catch (HibernateException e) {
 				try {
+					log.warn("Caught exception when trying to commit transaction. Rolling back.",e);
 					t.rollback();
-					throw new HibernateException("Caught exception trying to commit transaction",e);
-				}
-				catch (HibernateException e2) {
-					HibernateException e3 = new HibernateException("Got exception trying to rollback transaction after failure",e2);
+					((HttpServletResponse)response).sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,"");
+					throw new HibernateException(
+							"Caught exception trying to commit transaction", e);
+				} catch (HibernateException e2) {
+					log.error("Failed to rollback transaction after failing to commit",e2);
+					HibernateException e3 = new HibernateException(
+							"Got exception trying to rollback transaction after failure",
+							e2);
 					e3.addSuppressed(e);
 					throw e3;
 				}
